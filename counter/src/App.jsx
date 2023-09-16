816a11c10f8335a92aac4e8a86193765851ca724
Praveenskg <Praveensinghiics@gmail.com>
2023-09-16 23:38:21 +0530

    Understanding the Hooks

---
+++ counter/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const addValue = () => {
+    if (count === 20) {
+    } else {
+      setCount(count + 1);
+    }
+  };
+
+  const removeValue = () => {
+    if (count === 0) {
+    } else {
+      setCount(count - 1);
+    }
+  };
+  return (
+    <>
+      <h1>React Counter </h1>
+      <h2>Counter value : {count}</h2>
+      <button onClick={addValue}>Add Value</button>
+
+      <button onClick={removeValue}>Remove Value</button>
+    </>
+  );
+}
+
+export default App;
