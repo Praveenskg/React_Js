e4eeff3b8dc8b8ad3f3b12c00cd7b75bd0f82240
Praveenskg <Praveensinghiics@gmail.com>
2023-09-18 00:36:03 +0530

    :tada: added tailwind

--- tailwindprops/src/App.jsx
+++ tailwindprops/src/App.jsx
@@ -1,35 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./App.css";
+import Card from "./Components/Card";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">Tailwind </h1>
+      <Card name="Praveen" btnText="Click Me" />
+      <Card name="Pranjal" />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
