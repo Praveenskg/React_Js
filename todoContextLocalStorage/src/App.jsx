ca8d1ba58498d4424f36e3f29e14b1bfbc8c8869
Praveen Singh <Praveensinghiics@gmail.com>
2023-09-29 01:12:48 +0530

    :fire: Removed index file

--- todoContextLocalStorage/src/App.jsx
+++ todoContextLocalStorage/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { TodoProvider } from "./contexts";
+import { TodoProvider } from "./contexts/TodoContext";
 import TodoForm from "./Components/TodoForm";
 import TodoItem from "./Components/TodoItem";
 import "./App.css";
